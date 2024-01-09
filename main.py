6be059c65ff6a44834f00dbfd1ffffbe3c77e4aa
Simone Arena <122620587+WasamiKirua@users.noreply.github.com>
2024-01-09 16:01:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ async def main():
         os.makedirs(f"{main_folder}/chapters", exist_ok=True)
         os.makedirs(f"{main_folder}/CBZ_files", exist_ok=True)
         # Fetch chapters
-        # chapters_list = await Fetcher.fetcher_chapters(url_input)
-        # for chapter_link in reversed(chapters_list):
+        chapters_list = await Fetcher.fetcher_chapters(url_input)
+        for chapter_link in reversed(chapters_list):
             # Destination folder's name
-            # chapter_name = chapter_link.split('/')[-1]
+            chapter_name = chapter_link.split('/')[-1]
             # Add safe wait
-            # await asyncio.sleep(delay=5)
-            # images_list = Processor.process_chapters(chapter_link)
-            # Downloader.download_image(images_list, chapter_name, main_folder=f"{main_folder}/chapters")
+            await asyncio.sleep(delay=5)
+            images_list = Processor.process_chapters(chapter_link)
+            Downloader.download_image(images_list, chapter_name, main_folder=f"{main_folder}/chapters")
             #await asyncio.sleep(3)  # Wait for 5 seconds between downloads
         Makercbz.create_cbz(main_folder=f"{main_folder}/chapters", cbz_folder=f"{main_folder}/CBZ_files")
 asyncio.run(main())
